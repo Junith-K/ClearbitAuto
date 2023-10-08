8a0e4c5aa836e372e652aa2015771b0522cc4d2e
Junith-K <junithjunith28@gmail.com>
2023-10-08 16:02:23 +0530

    added links

--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,9 @@
-function composeEmail(email, subject, person_name, company_name) {
+function composeEmail(email, subject, person_name, company_name, checkedornot, jobLink, jobTitle) {
     return new Promise((resolve)=> {
-        document.querySelector('input.agP.aFw').value = email
+        document.querySelector('input.agP.aFw').value = "junithkumar.r20@iiits.in"
+        if(checkedornot=="true"){
+          subject = subject.replace("Software or Frontend Engineer",jobTitle);
+        }
         document.querySelector('input[name="subjectbox"]').value = subject
         const divElement = document.querySelector('div.Am.Al.editable.LW-avf.tS-tW')
         const signatureDiv = divElement.querySelector('#mt-signature');
@@ -12,74 +15,75 @@ function composeEmail(email, subject, person_name, company_name) {
 
         //important
         //ceo
-        // divElement.innerHTML = `https://docs.google.com/forms/d/e/1FAIpQLScYENcU2ZPmi0E9Guw7xXYYW4TrnZU3R4zw_R2MWdlgDt3D_A/viewform
-        // <p>Dear ${person_name},</p>
-        
-        // <p>I hope you're doing well. I'm reaching out because I'm genuinely impressed with ${company_name.charAt(0).toUpperCase() + company_name.slice(1)}'s reputation and impact in the industry.</p> 
-        
-        // <p>I'm a student at IIIT Sri City with a passion for Software Engineering, especially in Technologies like ReactJS, NextJS, NodeJS, ExpressJS and MongoDB. You can check out my Portfolio to get a good idea of my skills which you can explore at this link: <a href="https://portfolio-juni.vercel.app/">Portfolio_Juni</a>.</p>
-        
-        // <p>Additionally, I have one year of practical experience gained through internships where I worked on various web development projects. I'm now looking for an Software/Frontend Engineer opportunity to further expand my skills and contribute effectively to an awesome team like yours.</p>
-        
-        // <p>If there's an opportunity for a Software/Frontend Engineer at ${company_name.charAt(0).toUpperCase() + company_name.slice(1)}, I'd love to be considered. I believe this experience would be a great fit for both of us.</p>
+        if(checkedornot=="false"){
+          divElement.innerHTML = `
+          <p>Dear ${person_name},</p>
+          
+          <p>I hope you're doing well. I'm reaching out because I'm genuinely impressed with ${company_name.charAt(0).toUpperCase() + company_name.slice(1)}'s reputation and impact in the industry.</p> 
+          
+          <p>I'm a student at IIIT Sri City with a passion for Software Engineering, especially in Technologies like ReactJS, NextJS, NodeJS, ExpressJS and MongoDB. You can check out my Portfolio to get a good idea of my skills which you can explore at this link: <a href="https://portfolio-juni.vercel.app/">Portfolio_Juni</a>.</p>
+          
+          <p>Additionally, I have one year of practical experience gained through internships where I worked on various web development projects. I'm now looking for a Software or Frontend Engineer opportunity to further expand my skills and contribute effectively to an awesome team like yours.</p>
+          
+          <p>If there's an opportunity for a Software or Frontend Engineer at ${company_name.charAt(0).toUpperCase() + company_name.slice(1)}, I'd love to be considered. I believe this experience would be a great fit for both of us.</p>
 
-        // <p>Thank you for your time, and I've attached my resume for your reference.</p>
-        
-        // <a href="https://drive.google.com/file/d/1LQoGXfPWFCU4vheV0o5_58vwmJVwFnQd/view">Resume_Junith</a>
+          <p>Thank you for your time, and I've attached my resume for your reference.</p>
+          
+          <a href="https://drive.google.com/file/d/1LQoGXfPWFCU4vheV0o5_58vwmJVwFnQd/view">Resume_Junith</a>
 
-        // <br>
+          <br>
 
-        // <p>Warm regards,<br>Junith<br>+919346723027</p>`;
-
-        //referral
-        divElement.innerHTML = `
-        <p>Dear ${person_name},</p>
-        
-        <p>I hope you're doing well. I'm reaching out because I'm genuinely impressed with ${company_name.charAt(0).toUpperCase() + company_name.slice(1)}'s reputation and impact in the industry.</p> 
-        
-        <p>I'm a student at IIIT Sri City with a passion for Software Engineering. You can check out my Portfolio to get a good idea of my skills which you can explore at this link: <a href="https://portfolio-juni.vercel.app/">Portfolio_Juni</a>.</p>
-        
-        <p>Additionally, I have one year of practical experience gained through internships where I worked on various web development projects. I'm now looking for an Software Engineer Internship opportunity to further expand my skills and contribute effectively to an awesome team like yours.</p>
-        
-        <p>I would love to get a referral from your side for the Software Engineer Intern role at ${company_name.charAt(0).toUpperCase() + company_name.slice(1)}. I believe this experience would be a great fit for me.</p>
-
-        <p>Thank you for your time, and I've attached my Resume and the Job Link for your reference.</p>
-        
-        <a href="https://drive.google.com/file/d/1LQoGXfPWFCU4vheV0o5_58vwmJVwFnQd/view">Resume_Junith</a>
-
-        <br>
-
-        <a href="https://wellfound.com/l/2zpLXL">Job_Link</a>
-
-        <br>
-
-        <p>Warm regards,<br>Junith<br>+919346723027</p>`;
+          <p>Warm regards,<br>Junith<br>+919346723027</p>`;
+        }else{
+          
+          //referral
+          divElement.innerHTML = `
+          <p>Dear ${person_name},</p>
+          
+          <p>I hope you're doing well. I'm reaching out because I'm genuinely impressed with ${company_name.charAt(0).toUpperCase() + company_name.slice(1)}'s reputation and impact in the industry.</p> 
+          
+          <p>I'm a student at IIIT Sri City with a passion for Software Engineering. You can check out my Portfolio to get a good idea of my skills which you can explore at this link: <a href="https://portfolio-juni.vercel.app/">Portfolio_Juni</a>.</p>
+          
+          <p>Additionally, I have one year of practical experience gained through internships where I worked on various web development projects. I'm now looking for an ${jobTitle} opportunity to further expand my skills and contribute effectively to an awesome team like yours.</p>
+          
+          <p>I would love to get a referral from your side for the ${jobTitle} role at ${company_name.charAt(0).toUpperCase() + company_name.slice(1)}. I believe this experience would be a great fit for me.</p>
+  
+          <p>Thank you for your time, and I've attached my Resume and the Job Link for your reference.</p>
+          
+          <a href="https://drive.google.com/file/d/1LQoGXfPWFCU4vheV0o5_58vwmJVwFnQd/view">Resume_Junith</a>
+  
+          <br>
+  
+          <a href="${jobLink}">Job_Link</a>
+  
+          <br>
+  
+          <p>Warm regards,<br>Junith<br>+919346723027</p>`;
+        }
 
         const divElement1 = document.querySelector('.T-I.J-J5-Ji.aoO.v7.T-I-atl.L3');
         divElement1.addEventListener('click', function onClick(){
             divElement1.removeEventListener('click', onClick);
             resolve();
         })
+
+        //CLICK
+        // setTimeout(()=>{
+        //     const clickEvent = new MouseEvent("click", {
+        //         bubbles: true,
+        //         cancelable: true,
+        //         view: window,
+        //         buttons: 1,
+        //     });
+        //     divElement1.dispatchEvent(clickEvent);
+        // },5000)
     })
-    
-    
-    //CLICK
-    // const divElement1 = document.querySelector('.T-I.J-J5-Ji.aoO.v7.T-I-atl.L3');
-    // setTimeout(()=>{
-    //     const clickEvent = new MouseEvent("click", {
-    //         bubbles: true,
-    //         cancelable: true,
-    //         view: window,
-    //         buttons: 1,
-    //     });
-    //     divElement1.dispatchEvent(clickEvent);
-    // },5000)
 }
   
 let namui="";
-let subject = "Expressing Interest in Software Engineer Intern Opportunity";
+let subject = "Expressing Interest in Software or Frontend Engineer Opportunity";
 
-async function checkForElement1(format, gmail, domain) {
+async function checkForElement1(format, gmail, domain, checkedornot, jobLink, jobTitle) {
     const targetElements = document.querySelectorAll(".cb-mb-0.cb-mt-0.cb-text-lg.cb-font-medium");
     
     if (targetElements.length) {
@@ -103,12 +107,12 @@ async function checkForElement1(format, gmail, domain) {
         let name = person_name.split(" ")
         let email = name[0].toLowerCase()+"."+name[name.length - 1].toLowerCase()+"@"+com_gmail.toLowerCase()+"."+domain_name;
         console.log(email)
-        await composeEmail(email, subject, person_name, company_name);
+        await composeEmail(email, subject, person_name, company_name, checkedornot, jobLink, jobTitle);
       }
     }
 }
 
-async function checkForElement2(format, gmail, domain) {
+async function checkForElement2(format, gmail, domain, checkedornot, jobLink, jobTitle) {
     const targetElements = document.querySelectorAll(".cb-mb-0.cb-mt-0.cb-text-lg.cb-font-medium");
     
     if (targetElements.length) {
@@ -130,12 +134,12 @@ async function checkForElement2(format, gmail, domain) {
           let name = person_name.split(" ")
           let email = name[0][0].toLowerCase()+name[name.length - 1].toLowerCase()+"@"+com_gmail.toLowerCase()+"."+domain_name;
           console.log(email)
-          await composeEmail(email, subject, person_name, company_name);
+          await composeEmail(email, subject, person_name, company_name, checkedornot, jobLink, jobTitle);
         }
       }
 }
 
-async function checkForElement3(format, gmail, domain) {
+async function checkForElement3(format, gmail, domain, checkedornot, jobLink, jobTitle) {
     const targetElements = document.querySelectorAll(".cb-mb-0.cb-mt-0.cb-text-lg.cb-font-medium");
     
     if (targetElements.length) {
@@ -157,12 +161,12 @@ async function checkForElement3(format, gmail, domain) {
           let name = person_name.split(" ")
           let email = name[0].toLowerCase()+"@"+com_gmail.toLowerCase()+"."+domain_name;
           console.log(email)
-          await composeEmail(email, subject, person_name, company_name);
+          await composeEmail(email, subject, person_name, company_name, checkedornot, jobLink, jobTitle);
         }
       }
 }
 
-async function checkForElement4(format, gmail, domain) {
+async function checkForElement4(format, gmail, domain, checkedornot, jobLink, jobTitle) {
     const targetElements = document.querySelectorAll(".cb-mb-0.cb-mt-0.cb-text-lg.cb-font-medium");
     
     if (targetElements.length) {
@@ -184,12 +188,12 @@ async function checkForElement4(format, gmail, domain) {
           let name = person_name.split(" ")
           let email = name[0].toLowerCase()+name[name.length - 1].toLowerCase()+"@"+com_gmail.toLowerCase()+"."+domain_name;
           console.log(email)
-          await composeEmail(email, subject, person_name, company_name);
+          await composeEmail(email, subject, person_name, company_name, checkedornot, jobLink, jobTitle);
         }
       }
 }
 
-async function checkForElement5(format, gmail, domain) {
+async function checkForElement5(format, gmail, domain, checkedornot, jobLink, jobTitle) {
   const targetElements = document.querySelectorAll(".cb-mb-0.cb-mt-0.cb-text-lg.cb-font-medium");
   
   if (targetElements.length) {
@@ -213,12 +217,12 @@ async function checkForElement5(format, gmail, domain) {
       let name = person_name.split(" ")
       let email = name[0].toLowerCase()+"."+name[name.length - 1][0].toLowerCase()+"@"+com_gmail.toLowerCase()+"."+domain_name;
       console.log(email)
-      await composeEmail(email, subject, person_name, company_name);
+      await composeEmail(email, subject, person_name, company_name, checkedornot, jobLink, jobTitle);
     }
   }
 }
 
-async function checkForElement6(format, gmail, domain) {
+async function checkForElement6(format, gmail, domain, checkedornot, jobLink, jobTitle) {
   const targetElements = document.querySelectorAll(".cb-mb-0.cb-mt-0.cb-text-lg.cb-font-medium");
   
   if (targetElements.length) {
@@ -242,30 +246,34 @@ async function checkForElement6(format, gmail, domain) {
       let name = person_name.split(" ")
       let email = name[0].toLowerCase()+name[name.length - 1][0].toLowerCase()+"@"+com_gmail.toLowerCase()+"."+domain_name;
       console.log(email)
-      await composeEmail(email, subject, person_name, company_name);
+      await composeEmail(email, subject, person_name, company_name, checkedornot, jobLink, jobTitle);
     }
   }
 }
   
-let checkInterval1, checkInterval2, checkInterval3, checkInterval4, checkInterval5;
+let checkInterval1, checkInterval2, checkInterval3, checkInterval4, checkInterval5, checkInterval6;
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     clearInterval(checkInterval1);
     clearInterval(checkInterval2);
     clearInterval(checkInterval3);
     clearInterval(checkInterval4);
     clearInterval(checkInterval5);
-  
+    clearInterval(checkInterval6);
+
+
     if (message.messageText === "f1") {
-      checkInterval1 = setInterval(() => checkForElement1(message.messageText,message.email,message.domain), 1000);
+      checkInterval1 = setInterval(() => checkForElement1(message.messageText,message.email,message.domain,message.checkedornot,message.jobLink,message.jobTitle), 1000);
     } else if (message.messageText === "f2") {
-      checkInterval2 = setInterval(() => checkForElement2(message.messageText,message.email,message.domain), 1000);
+      checkInterval2 = setInterval(() => checkForElement2(message.messageText,message.email,message.domain,message.checkedornot,message.jobLink,message.jobTitle), 1000);
     } else if (message.messageText === "f3") {
-      checkInterval3 = setInterval(() => checkForElement3(message.messageText,message.email,message.domain), 1000);
+      checkInterval3 = setInterval(() => checkForElement3(message.messageText,message.email,message.domain,message.checkedornot,message.jobLink,message.jobTitle), 1000);
     } else if (message.messageText === "f4") {
-      checkInterval4 = setInterval(() => checkForElement4(message.messageText,message.email,message.domain), 1000);
+      checkInterval4 = setInterval(() => checkForElement4(message.messageText,message.email,message.domain,message.checkedornot,message.jobLink,message.jobTitle), 1000);
     } else if (message.messageText === "f5") {
-      checkInterval5 = setInterval(() => checkForElement5(message.messageText,message.email,message.domain), 1000);
+      checkInterval5 = setInterval(() => checkForElement5(message.messageText,message.email,message.domain,message.checkedornot,message.jobLink,message.jobTitle), 1000);
     } else if (message.messageText === "f6") {
-      checkInterval5 = setInterval(() => checkForElement6(message.messageText,message.email,message.domain), 1000);
+      checkInterval6 = setInterval(() => checkForElement6(message.messageText,message.email,message.domain,message.checkedornot,message.jobLink,message.jobTitle), 1000);
+    } else if(message.messageText === "stop"){
+      console.log("Clearbit Auto Stopped!")
     }
 });
--- popup.js
+++ popup.js
@@ -3,29 +3,57 @@ import { getActiveTabURL } from "./utils.js";
 document.addEventListener("DOMContentLoaded", async () => {
   const activeTab = await getActiveTabURL();
   if (activeTab.url.includes("mail.google.com")) {
+    const email = document.getElementById('email');
+    const domain = document.getElementById('domain');
+    const checkbox = document.getElementById('myCheckbox');
     const buttons = document.querySelectorAll('.format');
-    const selectedButtonId = localStorage.getItem('selectedButtonId');
+    const myButton = document.getElementById('myButton');
+    const jobTitle = document.getElementById('jobTitle');
+    const jobLink = document.getElementById('jobLink');
+    const additionalFields = document.getElementById('additionalFields');
+    const myButtonStop = document.getElementById('myButtonStop');
 
+    
+    const selectedButtonId = localStorage.getItem('selectedButtonId');
     const getemail = localStorage.getItem('email');
     const getdomain = localStorage.getItem('domain');
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { action: `runMyFunction${selectedButtonId}`, messageText: selectedButtonId, email: getemail, domain: getdomain });
-    });
-    const email = document.getElementById('email');
-    email.value = getemail
+    const getcheckedornot = localStorage.getItem('checkedornot');
+    const getjobTitle = localStorage.getItem('jobTitle');
+    const getjobLink = localStorage.getItem('jobLink');
+
+    email.value = getemail?getemail:""
     email.addEventListener('input', function() {
       localStorage.setItem('email', email.value);
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { action: `runMyFunction${selectedButtonId}`, messageText: selectedButtonId, email: email.value, domain: domain.value });
-      });
     });
-    const domain = document.getElementById('domain');
-    domain.value = getdomain
+
+    domain.value = getdomain?getdomain:""
     domain.addEventListener('input', function() {
       localStorage.setItem('domain', domain.value);
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { action: `runMyFunction${selectedButtonId}`, messageText: selectedButtonId, email: email.value, domain: domain.value });
-      });
+    });
+
+    jobTitle.value = getjobTitle?getjobTitle:""
+    jobTitle.addEventListener('input', function() {
+      localStorage.setItem('jobTitle', jobTitle.value);
+    });
+
+    jobLink.value = getjobLink?getjobLink:""
+    jobLink.addEventListener('input', function() {
+      localStorage.setItem('jobLink', jobLink.value);
+    });
+
+    checkbox.checked = getcheckedornot=="true"?true:false;
+    if (checkbox.checked) {
+      additionalFields.style.display = 'block';
+    } else {
+      additionalFields.style.display = 'none';
+    }
+    checkbox.addEventListener('change', function() {
+      localStorage.setItem('checkedornot', checkbox.checked);
+      if (checkbox.checked) {
+        additionalFields.style.display = 'block';
+      } else {
+        additionalFields.style.display = 'none';
+      }
     });
     
     buttons.forEach(button => {
@@ -37,20 +65,35 @@ document.addEventListener("DOMContentLoaded", async () => {
         });
         button.classList.add('selected');
         const id = button.id;
-        
-        // Store the selected button's ID in local storage
         localStorage.setItem('selectedButtonId', id);
-        
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-          chrome.tabs.sendMessage(tabs[0].id, { action: `runMyFunction${id}`, messageText: id, email: email.value, domain: domain.value });
-        });
       });
       
-      // Set the selected class based on the stored ID
       if (button.id === selectedButtonId) {
         button.classList.add('selected');
       }
     });
+
+
+    myButton.addEventListener('click', function() {
+      const selectedButtonId = localStorage.getItem('selectedButtonId');
+      const getemail = localStorage.getItem('email');
+      const getdomain = localStorage.getItem('domain');
+      const getcheckedornot = localStorage.getItem('checkedornot');
+      const getjobTitle = localStorage.getItem('jobTitle');
+      const getjobLink = localStorage.getItem('jobLink');
+
+
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { action: `runMyFunction${selectedButtonId}`, checkedornot: getcheckedornot, jobTitle: getjobTitle, jobLink: getjobLink, messageText: selectedButtonId, email: getemail, domain: getdomain });
+      });
+    });
+
+    myButtonStop.addEventListener('click', function(){
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { action: `stop`, messageText: "stop"});
+      });
+    });
+
   } else {
     const container = document.getElementsByClassName("container")[0];
     container.textContent = 'This is not a Gmail page';
